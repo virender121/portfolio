12ddf47daccf8de4e6ed8e23e754623411306b2c
virender121 <119596715+virender121@users.noreply.github.com>
2023-04-20 06:03:47 +0530

    changes

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,3 +1,4 @@
+
 const ProjectCardData = [
     {
         imgsrc : "https://media.licdn.com/dms/image/D5622AQGY3BzHB9UG-Q/feedshare-shrink_800/0/1680960681978?e=1683763200&v=beta&t=Oe50Aj1vNKGkN255DwjKk_149VM43lrlEy3V9pftuck",
@@ -7,14 +8,14 @@ const ProjectCardData = [
         source: "https://github.com/virender121/CheckkoutMFE"
     },
     {
-        imgsrc : "https://scontent.fixc1-5.fna.fbcdn.net/v/t39.30808-6/340383197_966211964370643_3564763377379350819_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=tzI9qLX_cOwAX_TYvM8&_nc_ht=scontent.fixc1-5.fna&oh=00_AfDu1dU1cn_gTLqC0z263goLr_Gu7GN2cYnq8Gb90Uwkjg&oe=64377D57",
+        imgsrc : "https://scontent.fixc1-5.fna.fbcdn.net/v/t39.30808-6/340383197_966211964370643_3564763377379350819_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=inX6-b3AJvcAX9f4C__&_nc_ht=scontent.fixc1-5.fna&oh=00_AfD4VrC2Yrz_Kb00L5PG3Axk1im_a7XcCpIqYNSPdcm6Jg&oe=64455517",
         title : "Gym Website" ,
         text : "Developed a subscription-based gymnasium website with React JS, HTML5, JS, and CSS3, allowing users to access premium services and facilities with ease. Created a resposive for mobile view website.",
         view: "https://gym-website-git-main-virender121.vercel.app/",
         source: "https://github.com/virender121/GYM-WEBSITE"
     },
     {
-        imgsrc : "https://scontent.fixc1-5.fna.fbcdn.net/v/t39.30808-6/340527723_180039721542310_8104454183060169277_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=539zDyaxgFgAX_6Q6SM&_nc_oc=AQlG31UPWGlApXFW-15RgW3lwbV5FMEo0GsWNZfNzwxUHKeK7pR3QJn_FRb6vwYMx-KWbf7UOmQguuT6takZnp4p&_nc_ht=scontent.fixc1-5.fna&oh=00_AfANfHWxxSu0poVAtEE1V0EJwie7jwdbwwOnwhToBGURZA&oe=6437E455",
+        imgsrc : "https://scontent.fixc1-5.fna.fbcdn.net/v/t39.30808-6/340527723_180039721542310_8104454183060169277_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=AW3a5tgw400AX895NZT&_nc_oc=AQmxI11ScG504fjs9I4G1r3TNxdh3zNtsK5lM7kfkrD6NiZ7OtZ-TOymfHyU8-58IMKbvU1hQsLQ_bzYksdJYTEl&_nc_ht=scontent.fixc1-5.fna&oh=00_AfBfVIZdzc4BAtVBlU_VTTYXG0VMRu9ARPwaWmKbkJ9t3Q&oe=6445BC15",
         title : "Twitter Clone" ,
         text : "The user profile page is a crucial aspect of the Twitter Clone, as it provides users with a centralized location to view their tweets, followers, and other related information. The profile page is designed to be user-friendly and visually appealing, making it easy for users to navigate and interact with the platform.Created a mobile responsive website.",
         view: "https://twitter-clone-virender121.vercel.app/",
@@ -23,9 +24,16 @@ const ProjectCardData = [
     {
         imgsrc : "https://catalog.wlimg.com/4/803241/other-images/382224.jpg",
         title : "Himalayan Safar Travels" ,
-        text : "had worked on Himalayan Safar tourism website using React.JS for making user-interface and adding javascript to make interactive that website.I had made it for one for one of my friend with the help of senior developer. You can check it on internet.",
+        text : "had worked on Himalayan Safar tourism website using React.JS for making user-interface and adding javascript to make interactive that website.I had made it for one of my friend with the help of senior developer. You can check it on internet.",
         view: "http://www.himalayansafar.com/",
         source: ""
+    },
+    {
+        imgsrc : "https://scontent.fixc1-5.fna.fbcdn.net/v/t39.30808-6/342347875_787809009099422_329986705769777245_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=-lSFDt47DZ4AX8Aalb1&_nc_ht=scontent.fixc1-5.fna&oh=00_AfAYG7vz-VUErWpOPTCNK_S1YNv223N2oVEVV0C-sEg4mQ&oe=64447ED3",
+        title : "E-commerce Website" ,
+        text : "Created a fully responsive e- commerce website using reduxe toolkit library to store user data and getting data from library to authenticate user and used private routing. ",
+        view: "https://ecommercewebsite-virender121.vercel.app/",
+        source: "https://github.com/virender121/Assignment"
     }
 ]
 
